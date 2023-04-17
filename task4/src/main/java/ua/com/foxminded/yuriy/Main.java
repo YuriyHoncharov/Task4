015a94928b7885162d37d0c5919201d62ea4e3e2
Yuriy Honcharov <yuriyhoncharov@gmail.com>
2023-04-17 22:10:14 +0200

    Solved all threads

--- task4/src/main/java/ua/com/foxminded/yuriy/Main.java
+++ task4/src/main/java/ua/com/foxminded/yuriy/Main.java
@@ -1,14 +1,12 @@
 package ua.com.foxminded.yuriy;
 
-import java.util.ArrayList;
 import java.util.Map;
 
 public class Main {
-
 	public static void main(String[] args) {
-
+		
 		CharCollection charCount = new CharCollection();
-		String str = "Hello World!";
+		String str = "nice to meet you1.";
 		Map<String, Integer> letters = charCount.countUniqueCharacters(str);
 		System.out.println(charCount.printUniqueCharacter(letters));
 	}
